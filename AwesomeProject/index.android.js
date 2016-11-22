f829dcc6cd6940513d0ca2cb96fa931263d0153e
Jameson Welling <jamowelling@gmail.com>
2016-11-22 13:21:06 -0700

    initial commit

---
+++ AwesomeProject/index.android.js
@@ -0,0 +1,37 @@
+// Import some code we need
+import React, { Component } from 'react';
+import Moment from 'moment';
+import { View, AppRegistry, Text, StyleSheet } from 'react-native';
+import DayItem from './src/day-item';
+
+// Create a react Component
+export default class Weekdays extends Component {
+  days() {
+    let daysItems = []
+
+    for(var i = 0; i < 7; i++) {
+      let day = Moment().add(i, 'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+    return daysItems
+  }
+    render() {
+      return <View style={styles.container}>
+        {this.days()}
+      </View>
+    }
+  };
+
+// Style the React component
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // Moves stuff heightwise
+    alignItems: 'center' // Moves stuff widthwise
+  }
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('AwesomeProject', () => Weekdays );
---
+++ AwesomeProject/src/day-item.js
@@ -0,0 +1,44 @@
+// Import some code we need
+import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+// Create our Component
+export default class DayItem extends Component {
+  color() {
+    let opacity = 1 / this.props.daysUntil
+    return 'rgba(0, 0, 0, '+ opacity +' )'
+  }
+  fontWeight() {
+    return ( 7 - this.props.daysUntil) * 100;
+  }
+  fontSize() {
+    return 60 - 6 * this.props.daysUntil
+  }
+  lineHeight() {
+    return 70 - 4 * this.props.daysUntil;
+  }
+  style() {
+    return {
+      color: this.color(),
+      fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  }
+
+  render() {
+    return <Text style={this.style()}>
+      {this.props.day}
+    </Text>
+  }
+}
+
+// Style our Component
+const styles = StyleSheet.create({
+  day: {
+    fontSize: 18,
+    color: '#0000FF'
+  }
+})
+
+// Make this code available elsewhere
